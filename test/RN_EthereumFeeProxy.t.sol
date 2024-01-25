46ca780e6d4ce1bd63837af20cfc06b456059754
pclaesen <pieterclaesen@gmail.com>
2024-01-25 14:10:26 +0000

    Add test for issue #1149, feeAddress can't be a smart contract

--- test/RN_EthereumFeeProxy.t.sol
+++ test/RN_EthereumFeeProxy.t.sol
@@ -16,11 +16,12 @@ contract RN_EthereumFeeProxyTest is Test {
 
     address RNEthereumFeeProxyaddress = 0xfCFBcfc4f5A421089e3Df45455F7f4985FE2D6a8; //Mainnet address for EthereumFeeProxy contract
 
-    function test_PaymentWithFee() public payable {
+    address alice = vm.addr(1);
+    address payable bob = payable(vm.addr(2));
+    address payable feeAddress = payable(vm.addr(3));
+    address payable smartContractReceiver = payable(0xae7ab96520DE3A18E5e111B5EaAb095312D7fE84); //Lido stETH token contract address
 
-        address alice = vm.addr(1);
-        address payable bob = payable(vm.addr(2));
-        address payable feeAddress = payable(vm.addr(3));
+    function test_PaymentWithFee() public payable {
 
         vm.deal(alice, 100 ether);
         bytes memory paymentReference1 = "0x494e56332d32343001";
@@ -36,10 +37,6 @@ contract RN_EthereumFeeProxyTest is Test {
 
     function test_PaymentZeroFee() public payable {
 
-        address alice = vm.addr(1);
-        address payable bob = payable(vm.addr(2));
-        address payable feeAddress = payable(vm.addr(3));
-
         vm.deal(alice, 100 ether);
         bytes memory paymentReference1 = "0x494e56332d32343001";
 
@@ -49,6 +46,35 @@ contract RN_EthereumFeeProxyTest is Test {
             paymentReference1,
             0 ether,
             feeAddress       
+        );
+    }
+
+    function test_PaymentToSmartContractAddress() external payable{
+        
+        vm.deal(alice, 100 ether);
+        bytes memory paymentReference1 = "0x494e56332d32343001";
+
+        vm.prank(alice);
+        IEthereumFeeProxy(RNEthereumFeeProxyaddress).transferWithReferenceAndFee{value: 10 ether} ( 
+            smartContractReceiver,
+            paymentReference1,
+            1 ether,
+            bob
+        );
+    }
+
+    function test_RevertWhenFeeToSmartContractAddress() external payable{
+        
+        vm.deal(alice, 100 ether);
+        bytes memory paymentReference1 = "0x494e56332d32343001";
+
+        vm.prank(alice);
+        vm.expectRevert();
+        IEthereumFeeProxy(RNEthereumFeeProxyaddress).transferWithReferenceAndFee{value: 10 ether} ( 
+            bob,
+            paymentReference1,
+            1 ether,
+            smartContractReceiver
         );
     }
 }
